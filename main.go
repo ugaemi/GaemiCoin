0947d8481d4d044592f4b705652e9caffa9ddd38
ugaemi <u.gaemi@gmail.com>
2021-07-25 14:40:13 +0900

    refactor: Refactoring

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/ugaemi/gaemicoin/blockchain"
-	"log"
-	"net/http"
-	"text/template"
-)
-
-const port string = ":4000"
-const templateDir string = "templates/"
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks []*blockchain.Block
-}
-
-func home (rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
+import "github.com/ugaemi/gaemicoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
