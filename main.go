303db1807ae43b31daf1bcf5745a3540647dea65
ugaemi <u.gaemi@gmail.com>
2021-07-25 10:17:11 +0900

    feat: #5.1 Rendering Templates

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/ugaemi/gaemicoin/blockchain"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks []*blockchain.Block
+}
+
 func home (rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!")
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
+	tmpl.Execute(rw, data)
 }
 
 func main() {
