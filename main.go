afe70082a9d874fed6b1597eff49cf7d843bddfc
ugaemi <u.gaemi@gmail.com>
2021-07-26 23:19:39 +0900

    feat: #6.0 Setup

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/ugaemi/gaemicoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ugaemi/gaemicoin/utils"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL string
+	Method string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription {
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
